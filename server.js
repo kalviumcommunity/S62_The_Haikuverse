5a46c738f95e09ab17c6bc561f593971bdeff5f2
Rishitha1512 <katragadda.rishitha@kalvium.community>
2025-01-09 18:08:25 +0530

    completed mongodb client

--- server.js
+++ server.js
@@ -2,21 +2,22 @@ if (process.env.NODE_ENV !== 'PRODUCTION') {
   require('dotenv').config();
 }
 const express = require('express');
-const connectDatabase = require('./DB/database.js');
 
-const mongoose = require('mongoose')
-
+const { getDbFn, connection } = require('./DB/mongo-client.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
 
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   // Check the connection status
-  const dbStatus = mongoose.connection.readyState === 1 ? 'Connected' : 'Not Connected';
+  const checkStatus = await connection.connect();
+  const readyState = connection.topology.isConnected()
+    ? 'connected'
+    : 'disconnected';
 
   // Send the status as a response
-  res.send(`Database Connection Status: ${dbStatus}`);
+  res.send(`Database Connection Status: ${readyState}`);
 });
 
 app.get('/ping',(request,response)=>{
@@ -24,6 +25,6 @@ app.get('/ping',(request,response)=>{
 });
 
 app.listen(port,()=>{
-    connectDatabase();
+    // connectDatabase();
     console.log(`Your server is running on http://localhost:${port}`);
 });
