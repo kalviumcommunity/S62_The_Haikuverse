9a3f9246d87ed96bd89795d722aea3af209a9035
Rishitha1512 <katragadda.rishitha@kalvium.community>
2025-01-28 12:03:17 +0530

    completed ASAP Project - Using component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Poem from './Poems/PoemCard.jsx'
+// import App from './App.jsx'
+// import Poem from './Poems/PoemCard.jsx'
+import Users from './Users.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Poem />
+    {/* <App />
+    <Poem /> */}
+    <Users/>
   </StrictMode>,
 )
