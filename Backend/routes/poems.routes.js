146c62ede6d37948cb0a1b90a71bb34a652bf552
Rishitha1512 <katragadda.rishitha@kalvium.community>
2025-03-11 22:39:22 +0530

    completed ASAP Project - Using SQL database

--- Backend/routes/poems.routes.js
+++ Backend/routes/poems.routes.js
@@ -1,47 +1,49 @@
 const express = require("express");
-const { getDB } = require("../DB/mongo-client");
-const { ObjectId } = require("mongodb");
+const mysqlDB = require("../DB/mysqlDB"); 
 const app = express.Router();
 
 app.use(express.json());
 
+app.post("/poem", (req, res) => {
+  const { userId, title, content } = req.body;
 
-app.post("/poem", async (req, res) => {
-    try {
-      const { userId, title, content } = req.body;
 
-      if (!ObjectId.isValid(userId)) {
-        return res.status(400).send({ message: "Invalid user ID" });
+  if (!userId || isNaN(userId)) {
+    return res.status(400).send({ message: "Invalid user ID" });
+  }
+
+
+  const insertPoemQuery = 'INSERT INTO poems (user_id, title, content) VALUES (?, ?, ?)';
+  mysqlDB.query(insertPoemQuery, [userId, title, content], (err, result) => {
+    if (err) {
+      return res.status(500).send({ message: err.message });
+    }
+
+    const poemId = result.insertId;
+
+    const updateUserQuery = 'UPDATE users SET poems = CONCAT_WS(",", poems, ?) WHERE id = ?';
+    mysqlDB.query(updateUserQuery, [poemId, userId], (err, result) => {
+      if (err) {
+        return res.status(500).send({ message: err.message });
       }
-  
-      const db = await getDB("poems");
 
-      const newPoem = {
-        title,
-        content
-      };
-      const insertPoem = await db.insertOne(newPoem);
-      const poemId = insertPoem.insertedId;
-  
-      const userDb = await getDB("User");
-      const updateUser = await userDb.updateOne(
-        { _id: new ObjectId(userId) },
-        { $push: { poems: poemId } } 
-      );
-  
-      if (updateUser.modifiedCount === 0) {
+      if (result.affectedRows === 0) {
         return res.status(404).send({ message: "User not found" });
       }
-  
-      const createdPoem = await db.findOne({ _id: poemId });
-  
-      return res.status(201).send({
-        message: "Poem created and added to user",
-        poem: createdPoem,
-      });
-    } catch (er) {
-      return res.status(500).send({ message: er.message });
-    }
-  });
 
-  module.exports = app;
+      const getPoemQuery = 'SELECT * FROM poems WHERE id = ?';
+      mysqlDB.query(getPoemQuery, [poemId], (err, poemResult) => {
+        if (err) {
+          return res.status(500).send({ message: err.message });
+        }
+
+        return res.status(201).send({
+          message: "Poem created and added to user",
+          poem: poemResult[0],
+        });
+      });
+    });
+  });
+});
+
+module.exports = app;
